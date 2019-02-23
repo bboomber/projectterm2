1016057ebff1a418626e533028245e425e9eee4a
baboom <58070035@kmitl.ac.th>
2019-02-23 14:30:35 +0700

    add home path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from user import views as user_views
+from . import views as mysite_views
 
 urlpatterns = [
+    path('', mysite_views.showhome, name='showhome'),
     path('employee/', include('employee_control.urls')),
     path('signup/', user_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
