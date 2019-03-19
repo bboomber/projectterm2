9b7ed2181f4b93a9c3609691325af2d6478a1a00
baboom <58070035@kmitl.ac.th>
2019-03-19 15:49:10 +0700

    แก้ส่วน DB ของออกกรมธรรม์
    แก้ให้รับข้อมูลมาจากอันอื่นไง อีเวง

--- mysite/insurance/views.py
+++ mysite/insurance/views.py
@@ -6,7 +6,8 @@ from .models import Insure
 from django.contrib.auth import login as auth_login, authenticate, login
 from django.contrib.auth.models import User
 from employee_control.models import Employee
-from customer.models import Car
+from customer.models import Car, Customer
+from package_control.models import Package
 
 
 def viewInsureDetail(request, id):
@@ -20,21 +21,28 @@ def sellInsure(request):
         form = InsureForm(request.POST)
         if form.is_valid():
             doc_nbr = form.cleaned_data.get('doc_nbr')
-            package_id = form.cleaned_data.get('package_id')
-            
+            # ------------------------------------------
+            pack_id = form.cleaned_data.get('package_id')
+            package = Package.objects.get(package_id=pack_id)
+            # ------------------------------------------
             car_id = form.cleaned_data.get('car_id')
             car = Car.objects.get(car_id=car_id)
-
+            # ------------------------------------------
+            cus_id = form.cleaned_data.get('cus_id')
+            cus = Customer.objects.get(cus_id=cus_id)
+            # ------------------------------------------
             company_order = form.cleaned_data.get('company_order')
             price = form.cleaned_data.get('price')
             total_price = form.cleaned_data.get('total_price')
             post_date = form.cleaned_data.get('post_date')
-
+            # ------------------------------------------
             employee = Employee.objects.get(id=request.user.id)
-
+            # ------------------------------------------
             ins = Insure(doc_nbr=doc_nbr, agent_code=employee,
-                         car_id=car,
-                         car_number=car.car_number, company_order=company_order,
+                         package_id=package,
+                         car_id=car, car_number=car.car_number,
+                         cus_id=cus,
+                         company_order=company_order,
                          price=price, total_price=total_price,
                          post_date=post_date)
             ins.save()
