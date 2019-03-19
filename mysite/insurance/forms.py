cb713e563575a05c9b769c6eb11014334b813ebb
baboom <58070035@kmitl.ac.th>
2019-03-19 15:24:48 +0700

    sell insure update
    แก้ให้รับข้อมูลแบบเสิรช

--- mysite/insurance/forms.py
+++ mysite/insurance/forms.py
@@ -3,24 +3,32 @@ from django.shortcuts import get_object_or_404
 from django.forms import DateTimeField
 from .models import Insure
 from package_control.models import Package
+from customer.models import Car, Customer
+
+pack_list = Package.objects.order_by('id')
+pack_choice = []
+for pack in pack_list:
+    pack_choice.append((pack.package_id, pack.package_name))
+
+car_list = Car.objects.order_by('id')
+car_choice = []
+for car in car_list:
+    car_choice.append((car.id, car.car_number))
+
+cus_list = Customer.objects.order_by('id')
+cus_choice = []
+for cus in cus_list:
+    cus_choice.append((cus.id, cus.fname))
 
 
 class InsureForm(forms.Form):
-    pack_list = Package.objects.order_by('id')
-    pack_choice = []
-    for pack in pack_list:
-        pack_choice.append((pack.package_id, pack.package_name))
-
     doc_nbr = forms.CharField(max_length=30, label="รหัสกรมธรรม์")
-
-    # package_id = forms.ChoiceField(choices=pack_choice, label="เลือกแพ็คเกจ")
-    package_id = forms.ChoiceField(choices=pack_choice, label="เลือกแพ็คเกจ", widget=forms.Select(attrs={'class': 'selectpicker', 'data-live-search': 'true'}))
-    
-    car_id = forms.CharField(max_length=30, label="รหัสรถยนต์")
-    
-    cus_id = forms.CharField(max_length=30, label="รหัสลูกค้า")
-
-    car_number = forms.CharField(max_length=30, label="ทะเบียนรถ")
+    package_id = forms.ChoiceField(choices=pack_choice, label="เลือกแพ็คเกจ", widget=forms.Select(
+        attrs={'class': 'selectpicker', 'data-live-search': 'true'}))
+    cus_id = forms.ChoiceField(choices=cus_choice, label="เลือกลูกค้า", widget=forms.Select(
+        attrs={'class': 'selectpicker', 'data-live-search': 'true'}))
+    car_id = forms.ChoiceField(choices=car_choice, label="เลือกรถยนต์", widget=forms.Select(
+        attrs={'class': 'selectpicker', 'data-live-search': 'true'}))
     company_order = forms.CharField(max_length=30, label="บริษัทประกัน")
     price = forms.FloatField(label="ราคาสุทธิ")
     total_price = forms.FloatField(label="ราคารวม")
--- mysite/insurance/views.py
+++ mysite/insurance/views.py
@@ -6,6 +6,7 @@ from .models import Insure
 from django.contrib.auth import login as auth_login, authenticate, login
 from django.contrib.auth.models import User
 from employee_control.models import Employee
+from customer.models import Car
 
 
 def viewInsureDetail(request, id):
@@ -20,8 +21,10 @@ def sellInsure(request):
         if form.is_valid():
             doc_nbr = form.cleaned_data.get('doc_nbr')
             package_id = form.cleaned_data.get('package_id')
+            
             car_id = form.cleaned_data.get('car_id')
-            car_number = form.cleaned_data.get('car_number')
+            car = Car.objects.get(car_id=car_id)
+
             company_order = form.cleaned_data.get('company_order')
             price = form.cleaned_data.get('price')
             total_price = form.cleaned_data.get('total_price')
@@ -30,7 +33,8 @@ def sellInsure(request):
             employee = Employee.objects.get(id=request.user.id)
 
             ins = Insure(doc_nbr=doc_nbr, agent_code=employee,
-                         car_number=car_number, company_order=company_order,
+                         car_id=car,
+                         car_number=car.car_number, company_order=company_order,
                          price=price, total_price=total_price,
                          post_date=post_date)
             ins.save()
