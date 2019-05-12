3212dda01b98da684466aefbe2d16d781c2bc596
baboom <58070035@kmitl.ac.th>
2019-05-13 02:15:54 +0700

    power BI อันใหม่จ้า

--- mysite/employee_control/static/employee_control/js/remindJS.js
+++ mysite/employee_control/static/employee_control/js/remindJS.js
@@ -2,16 +2,4 @@ $(document).ready(function() {
     $('#table').DataTable({
         "ordering": false
     });
-    $('#table1').DataTable();
-    $('#table2').DataTable();
-    $('#table3').DataTable();
-    $('#table4').DataTable();
-    $('#table5').DataTable();
-    $('#table6').DataTable();
-    $('#table7').DataTable();
-    $('#table8').DataTable();
-    $('#table9').DataTable();
-    $('#table10').DataTable();
-    $('#table11').DataTable();
-    $('#table12').DataTable();
 });
