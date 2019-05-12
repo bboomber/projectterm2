1842c7f300e32c2ff6cc8e9ef94ad2968cb0a8f9
parely <bb_pob@hotmail.com>
2019-05-13 01:48:51 +0700

    edit predict graph final by bell

--- mysite/employee_control/static/employee_control/js/predictGraphJS.js
+++ mysite/employee_control/static/employee_control/js/predictGraphJS.js
@@ -1,4 +1,4 @@
-function showGraph(months, insure_list, year) {
+function showGraph(months, insure_list) {
     var ctx = document.getElementById('predictChart').getContext('2d');
         var chart = new Chart(ctx, {
         // The type of chart we want to create
--- mysite/insurance/views.py
+++ mysite/insurance/views.py
@@ -61,15 +61,17 @@ def showReport(request):
 
 
 def showPredict(request):
-    insure_list = {}
     months = ['January', 'February', 'March', 'April', 'May', \
         'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    for year in range(2016, 2018):
-        insure_count = []
+    insure_list = {}
+    insure_count = []
+
+    for year in range(2015, 2017):
         for month in range(1, 13):
             ins_monthly_count = Insure.objects.filter(post_date__year=year, post_date__month=month).count()
             insure_count.append(ins_monthly_count)
-        insure_list[str(year)] = insure_count
+    
+    insure_list[str(2017)] = predictSale(insure_count)
 
     return render(request, 'insurance/showPredict.html', {'months': months, 'insure_list': insure_list})
 
@@ -97,3 +99,123 @@ def newCusSell(request):
         tranForm = TranferForm()
         
     return render(request, "insurance/newCusSell.html", {'pic': pic, 'tranForm': tranForm})
+
+# Predict Sale by year
+def predictSale(insure_list):
+    alpha = 0.1341
+    beta = 0.8575
+    gamma = 0.8082
+    value = insure_list
+
+    ls = int(0)
+    yl = int(0)
+
+    mad_avg  = int(0)
+    mape_avg = int(0)
+
+    sl = []
+    at = []
+    bt = []
+    st = []
+    ft = []
+
+    mad  = []
+    mape = []
+
+    count_at = int(1)
+    count_bt = int(0)
+    count_st = int(13)
+    count_stl = int(0)
+
+    #LS At
+    for l in range(12):
+        ls += value[l]
+    ls = ls/12
+    # print("Ls: ", ls)
+
+    #Yl+t-Yt
+    for y in range(0, 12, 1):
+        yl += value[12+y]-value[0+y]
+    # print(yl)
+
+    #Bs Tt
+    yl = yl/12**2
+    # print("Bs: ", yl)
+
+    #Sl St
+    for s in range(0, 12, 1):
+        sl.insert(s, value[s]-ls)
+    # print("St-L+m: ", sl)
+
+    #At
+    at.insert(0, alpha*(value[12]-sl[0])+(1-alpha)*(ls+yl))
+
+    #bt
+    bt.insert(0, beta*(at[0]-ls)+(1-beta)*yl)
+
+    #st
+    st.insert(0, gamma*(value[12]-at[0])+(1-gamma)*(sl[0]))
+
+    #At
+    for atl in range(13, 24, 1):
+        at.insert(count_at, alpha*(value[atl]-sl[count_at])+(1-alpha)*(at[count_at-1]+bt[count_at-1]))
+        
+
+        #bt
+        bt.insert(count_bt+1, beta*(at[count_bt+1]-at[count_bt])+(1-beta)*bt[count_bt])
+        count_bt += 1
+
+        #st
+        st.insert(count_at, gamma*(value[count_st]-at[count_at])+(1-gamma)*(sl[count_at]))
+        count_st += 1
+        count_at += 1
+
+
+    #At2
+    for atl in range(24, len(value), 1):
+        at.insert(count_at, alpha*(value[atl]-st[count_stl])+(1-alpha)*(at[count_at-1]+bt[count_at-1])) 
+
+        #bt2
+        bt.insert(count_bt+1, beta*(at[count_bt+1]-at[count_bt])+(1-beta)*bt[count_bt])
+        count_bt += 1
+
+        #st2
+        st.insert(count_at, gamma*(value[count_st]-at[count_at])+(1-gamma)*(st[count_stl]))
+        count_st += 1
+        count_at += 1
+        count_stl += 1
+
+    ft.insert(0, (ls+yl)+st[0])
+    for j in range(0, len(value)-13, 1):
+        ft.insert(j+1, (at[j]+bt[j])+st[j+1])
+
+    #print
+    for i in range(0, len(value)-12, 1):   
+        # print("Period:", 13+i, "| Y: ", value[i+12], "| At:", at[i], "| bt:", bt[i], "| St:", st[i], "| ft:", ft[i], "| mad:", abs(value[i+12]-ft[i]), "| mape:",(abs(value[i+12]-ft[i])*100)/value[i+12] )
+        mad.insert(i, abs(value[i+12]-ft[i]))
+        mape.insert(i, (mad[i]*100)/value[i+12])
+
+    #mad average
+    for m in mad:
+        mad_avg += m
+    print("mad avg: ", (mad_avg/len(mad)))
+
+    #mape average
+    for ma in mape:
+        mape_avg += ma
+
+    mape_avg = mape_avg/len(mape)
+    st_pd = []
+    pd = []
+   
+    #Predict
+    for m in range(len(st)-1, len(st)-13, -1):
+        st_pd.insert(0, (st[m]))
+        pd.insert(0, (at[len(at)-1]+bt[len(bt)-1])+st[m]+(m+1))
+
+    predict_sale = []
+    for j in range(0, 12, 1):
+        predict_sale.append(pd[j])
+        # print("month : ", j+1 , "| St: ", st_pd[j], "| Predict: ", pd[j])
+    
+    return predict_sale
