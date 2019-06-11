27e744a33cf0c93cf947597aa1ba5e9bdb489c7f
baboom <58070035@kmitl.ac.th>
2019-06-11 21:44:08 +0700

    แก้วันนี้

--- mysite/user/forms.py
+++ mysite/user/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.Field(label="ชื่อlogin", widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1 = forms.PasswordInput()
+    password2 = forms.Field(label="พาสเวิร์ด2", widget=forms.TextInput(attrs={'class':'form-control'}))
     fname = forms.CharField(max_length=30, label="ชื่อ")
     lname = forms.CharField(max_length=30, label="นามสกุล")
     id_card = forms.CharField(max_length=13, label="เลขบัตรประชาชน")
@@ -11,7 +14,6 @@ class UserRegisterForm(UserCreationForm):
     phone2 = forms.CharField(max_length=15, label="เบอร์โทรศัพท์2")
     email = forms.EmailField(label="อีเมลล์")
     address = forms.CharField(max_length=200, label="ที่อยู่")
-
     class Meta:
         model = User
         fields = ('username',
